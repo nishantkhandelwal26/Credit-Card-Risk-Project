db6bd9f5d99beadbad6842e873271b2b684639d9
nishantkhandelwal26 <nishantkhandelwal111@gmail.com>
2026-03-31 11:42:14 +0530

    add metrics

---
+++ src/evaluate.py
@@ -0,0 +1,25 @@
+from sklearn.metrics import(
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+    confusion_matrix,
+    classification_report
+)
+
+def evaluate_model(model, x_test, y_test) -> dict:
+    y_pred = model.predict(x_test)
+    y_prob = model.predict_proba(x_test)[:, 1]
+
+    metrics = {
+        "accuracy": float(accuracy_score(y_test, y_pred)),
+        "precision": float(precision_score(y_test, y_pred)),
+        "recall": float(recall_score(y_test, y_pred)),
+        "f1_score": float(f1_score(y_test, y_pred)),
+        "roc_auc": float(roc_auc_score(y_test, y_prob)),
+        "confusion_matrix": confusion_matrix(y_test, y_pred).tolist(),
+        "classification_report": classification_report(y_test, y_pred, output_dict=True)
+    }
+
+    return metrics
