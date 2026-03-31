ceb0eeafa2000ea2ae30d8f8437a07c3bee72797
nishantkhandelwal26 <nishantkhandelwal111@gmail.com>
2026-03-31 11:39:31 +0530

    add ColumnTransformer for columns

---
+++ src/preprocess.py
@@ -0,0 +1,25 @@
+from sklearn import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from skelearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.config import NUMERIC_FEATURES, CATEGORICAL_FEATURES
+
+def build_preprocessor() -> ColumnTransformer:
+    numeric_tansformer = Pipeline(steps = [
+        ('imputer', SimpleImputer(strategy = 'median')),
+        ('scaler', StandardScaler())
+    ])
+
+    categorical_transformer = Pipeline(steps = [
+        ('imputer', SimpleImputer(strategy = 'most_frequent')),
+        ('onehot', OneHotEncoder(handle_unknown = 'ignore'))
+        ])
+    
+    preprocessor = ColumnTransformer(transformers = [
+        ('num', numeric_tansformer, NUMERIC_FEATURES),
+        ('cat', categorical_transformer, CATEGORICAL_FEATURES)
+    ])
+
+    return preprocessor
+    
